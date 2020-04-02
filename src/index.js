3138b55bbe7e7df58662f5987d0888210fee45b0
rivofaustav <rivofaustav.rv@gmail.com>
2020-04-02 15:32:25 +0700

    add function response for answer box

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import QuestionBox from "./components/QuestionBox";
 
 class QuizBee extends Component{
   state = {
-    questionBank: []
+    questionBank: [],
+    score: 0,
+    responses: 0
   };
   getQuestions = () => {
     quizService().then(question => {
@@ -15,6 +17,16 @@ class QuizBee extends Component{
       });
     });
   };
+  computeAnswer = (answer, correctAnswer) => {
+    if (answer === correctAnswer){
+      this.setState({
+        score: this.state.score + 1
+      });
+    }
+    this.setState({
+      responses: this.state.responses < 5 ? this.state.responses + 1 : 5
+    });
+  }
   componentDidMount() {
     this.getQuestions();
   }
@@ -22,8 +34,8 @@ class QuizBee extends Component{
     return(
       <div className="container">
         <div className="title">QuizBee</div>
-        {this.state.questionBank.length > 0 && this.state.questionBank.map(({question, answers, correct, questionId}) => (
-        <QuestionBox question={question} options={answers} key={questionId}/>)
+        {this.state.questionBank.length > 0 && this.state.responses < 5 && this.state.questionBank.map(({question, answers, correct, questionId}) => (
+        <QuestionBox question={question} options={answers} key={questionId} selected={answer => this.computeAnswer(answer, correct)}/>)
           )}
       </div>
     )
